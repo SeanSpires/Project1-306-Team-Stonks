389d0ca0b2608cbaa40ea92f91af80697564ee37
Ryan <ryanlimenwei@gmail.com>
2019-08-06 13:32:16 +1200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/sample/Model/FileIO.java
@@ -0,0 +1,45 @@
+package sample.Model;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileIO {
+
+    public List<String> inputStringList = new ArrayList<>();
+
+    public void writeFile(String fileName){
+        File outputFile = new File(fileName + ".dot");
+
+        String currentFileName = fileName+".dot";
+
+        if(outputFile.exists()){
+            outputFile.delete();
+        }
+        try {
+            FileWriter fw = new FileWriter(currentFileName,true);
+
+            for(String s : inputStringList) {
+                fw.write(s + "\n");
+            }
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile(String fileName){
+        File currentFile = new File(fileName);
+
+        try (BufferedReader br = new BufferedReader((new FileReader(currentFile)))){
+            String line;
+            while ((line = br.readLine()) != null){
+                inputStringList.add(line);
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
