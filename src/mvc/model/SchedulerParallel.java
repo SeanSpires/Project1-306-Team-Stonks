ea8275ba5af49b00830085aca9b099c43f925b11
Sean Spires <34573028+SeanSpires@users.noreply.github.com>
2019-08-29 12:04:12 +1200

    Updated java doc for SchedulerParallel class

--- src/mvc/model/SchedulerParallel.java
+++ src/mvc/model/SchedulerParallel.java
@@ -12,33 +12,48 @@ import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
+
+/**
+ * The SchedulerParallel class is responsable for creating an optimal scheudle (Node). However, it unlike the scheduler class
+ * it creates an optimal schedule using multiple threads. 
+ */
 public class SchedulerParallel {
 	
-	private PriorityBlockingQueue<Node> openNodes;
+	private PriorityBlockingQueue<Node> openNodes; 
 	private ForkJoinPool forkJoinPool;
 	
+	// use atomic variables to make it threadsafe
 	private AtomicLong bestLowerBound;
 	private AtomicLong bestUpperBound;
 	private HashSet<Integer> closed;
 	
+	
+	/**
+     	* Create an optimal schedule using branch & bound A* algorithm.
+	* Takes in a list of unschedulded tasks and the number of processors to create an optimal
+	* schedule based off these two inputs. A menucontroller is passed in to update the frontend
+	* during runtime.
+	* This method also uses multiple threads to compute the an optimal schedule
+    	*/
 	public Node createOptimalSchedule(List<Task> tasks, int numProc, int numCores, MenuController controller) {
 
 		this.closed = new HashSet<>();
 		
 		this.openNodes = new PriorityBlockingQueue<>();
-		this.forkJoinPool = new ForkJoinPool(numCores);
+		this.forkJoinPool = new ForkJoinPool(numCores); //create as many threads as numcores
 		this.bestLowerBound = new AtomicLong();
 		this.bestUpperBound = new AtomicLong();
 
 		bestUpperBound.set(Long.MAX_VALUE);
 		
-		Node n = new Node();
+		Node n = new Node(); //create root node
 		n.setUnscheduledTasks(tasks);
 		openNodes.add(n);
 		ArrayList<SchedulerParallelTask> schedulerTasks = new ArrayList<SchedulerParallelTask>();
 		AtomicBoolean done = new AtomicBoolean();
 		done.set(false);
 		
+		// Add a SchedulerParallelTask object to each thread and then invoke that thread
 		for(int i = 0 ; i < numCores; i++) {
 			SchedulerParallelTask t = new SchedulerParallelTask(openNodes, numProc, bestLowerBound, bestUpperBound, closed, done, controller);
 			schedulerTasks.add(t);
@@ -47,10 +62,11 @@ public class SchedulerParallel {
 		
 		Node out = null;
 		for (SchedulerParallelTask t : schedulerTasks) {
-			Node joined = (Node) t.join();
+			Node joined = (Node) t.join(); //wait for each thread to finish
 			if(joined == null){
 				continue;
 			}
+			// find thread which returned node with the lowest upper bound
 			if(out == null || joined.getUpperBound() < out.getUpperBound()) {
 				out = joined;
 
